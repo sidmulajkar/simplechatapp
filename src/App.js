e11ef314f611282a0c215949e1a69a0047397103
Siddhant Mulajkar <mulajkarsid@gmail.com>
2021-03-19 13:41:31 +0530

    Added the Signin Signout and basic firebase function to make it more responsive and act as serverless message transfer with auth functions and basic css added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React, { useRef, useState } from 'react';
 import './App.css';
-import firebase from 'firebase';
+
+import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/analytics';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
@@ -16,23 +19,108 @@ firebase.initializeApp({
     appId: "1:806289217868:web:e2a640667c164733ad97e9"
 })
 
-function App() {
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
+
+export default function App() {
+
   //for user signin and signout
   const [ user ] = useAuthState(auth);
   
   
   return (
     <div className="App">
-      <header className="App-header">
-
-        <section>
-          //if the user is signedin show the chatroom or show the signin button
-          {user ? <ChatRoom /> : <SignIn />}
-        </section>
-
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+
     </div>
   );
 }
 
-export default App;
+function SignIn() {
+  const signInWithGoogle =  () => {
+    
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not use for any other purpose than for project!</p>
+    </>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+
+  const dummy = useRef();
+  const messageRef = firestore.collection('messages');
+  const query = messageRef.orderBy('createdAt').limit(25);
+
+  //can listen to data using the hooks 
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+  const[formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+    e.preventDefault();
+
+    const {uid, photoURL } = auth.currentUser;
+
+    await messageRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (
+    <>
+      <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+        <span ref={dummy}></span>
+
+      </main>
+
+      <form onSubmit={sendMessage}>
+        
+        <input value = {formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice"/>
+
+        <button type="submit" disabled={!formValue}>🕊️</button>
+      </form>
+    </>
+  )
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  return (<>
+    <div className = {`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
+}
